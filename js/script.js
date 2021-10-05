5ec1e255b90aaeb2fb928f2a4737c808350421cc
Zahin Zulkipli <zahin@Zahins-MacBook-Pro.local>
2021-10-05 16:56:40 +0800

    update index.html

--- js/script.js
+++ js/script.js
@@ -5,6 +5,11 @@ darkModeIcon = document.getElementById('darkModeIcon')
 titleColor = document.querySelectorAll('.color-title')
 bodyColor = document.querySelectorAll('.color-body')
 elementLogo = document.querySelectorAll('.element-logo')
+darkBorder = document.querySelectorAll('.dark-border')
+
+nextSlidesIcon = document.querySelectorAll('.carousel-control-next-icon')
+prevSlidesIcon = document.querySelectorAll('.carousel-control-prev-icon')
+slideIndicator = document.querySelectorAll('.carousel-indicators [data-bs-target]')
 
 
 // ? dark mode function
@@ -15,17 +20,21 @@ window.onload = (evt) => {
     document.querySelector('body').style.backgroundColor = '#1C1026'
     // text title color
     for (let i = 0; i < titleColor.length; i++) {
-        titleColor[i].style.color = 'white'
+        titleColor[i].style.color = '#fdfdfd'
     }
     // text body color
     for (let i = 0; i < bodyColor.length; i++) {
-        bodyColor[i].style.color = '#c2c2c2'
+        bodyColor[i].style.color = '#959595'
     }
     // dark mode icon
     darkModeIcon.innerHTML = '<i class="bi bi-moon"></i>'
     // png elements color
     for (let i = 0; i < elementLogo.length; i++) {
         elementLogo[i].style.filter = 'initial'
+    }
+    // dark mode border
+    for (let i = 0; i < darkBorder.length; i++) {
+        darkBorder[i].style.backgroundColor = '#0F0717'
     }
 }
 // dark mode upon switch onchange
@@ -40,7 +49,7 @@ function darkMode() {
             }
             // text body color
             for (let i = 0; i < bodyColor.length; i++) {
-                bodyColor[i].style.color = '#c2c2c2'
+                bodyColor[i].style.color = '#959595'
             }
             // dark mode icon
             darkModeIcon.innerHTML = '<i class="bi bi-moon"></i>'
@@ -48,12 +57,29 @@ function darkMode() {
             for (let i = 0; i < elementLogo.length; i++) {
                 elementLogo[i].style.filter = 'initial'
             }
+            // dark mode border
+            for (let i = 0; i < darkBorder.length; i++) {
+                darkBorder[i].style.backgroundColor = '#0F0717'
+            }
+            // bootstrap next and prev icon
+            for (let i = 0; i < nextSlidesIcon.length; i++) {
+                nextSlidesIcon[i].style.filter = 'initial'
+            }
+            for (let i = 0; i < prevSlidesIcon.length; i++) {
+                prevSlidesIcon[i].style.filter = 'initial'
+            }
+            // bootstrap slides indicator
+            for (let i = 0; i < slideIndicator.length; i++) {
+                slideIndicator[i].style.backgroundColor = 'white'
+                
+            }
+
             break;
             
     
         case false:
             // html body color
-            document.querySelector('body').style.backgroundColor = 'white'
+            document.querySelector('body').style.backgroundColor = '#fdfdfd'
             // text title color
             for (let i = 0; i < titleColor.length; i++) {
                 titleColor[i].style.color = '#3B4250'
@@ -67,6 +93,23 @@ function darkMode() {
             // png elements color
             for (let i = 0; i < elementLogo.length; i++) {
                 elementLogo[i].style.filter = 'invert(76%) sepia(23%) saturate(396%) hue-rotate(181deg) brightness(97%) contrast(94%)'
+            }
+            // dark mode border
+            for (let i = 0; i < darkBorder.length; i++) {
+                darkBorder[i].style.backgroundColor = 'white'
+                darkBorder[i].style.boxShadow = '0 0 10px #00000026'
+            }
+            // bootstrap next and prev icon
+            for (let i = 0; i < nextSlidesIcon.length; i++) {
+                nextSlidesIcon[i].style.filter = 'invert(60%) sepia(14%) saturate(540%) hue-rotate(191deg) brightness(99%) contrast(91%)'
+            }
+            for (let i = 0; i < prevSlidesIcon.length; i++) {
+                prevSlidesIcon[i].style.filter = 'invert(60%) sepia(14%) saturate(540%) hue-rotate(191deg) brightness(99%) contrast(91%)'
+            }
+            // bootstrap slides indicator
+            for (let i = 0; i < slideIndicator.length; i++) {
+                slideIndicator[i].style.backgroundColor = '#b7b7b7'
+                
             }
             break;
     }
